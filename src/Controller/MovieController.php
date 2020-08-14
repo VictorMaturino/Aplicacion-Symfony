774e0203b1fcf88e811c70cdb4f7b37ebe44b601
VictorMaturino <vic.matusan.56@gmail.com>
2020-08-14 12:35:20 -0500

    backend

---
+++ src/Controller/MovieController.php
@@ -0,0 +1,105 @@
+<?php
+namespace App\Controller;
+
+use App\Entity\Movie;
+use App\Repository\MovieRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+
+class MovieController extends ApiController
+{
+    /**
+    * @Route("/movies", methods="GET")
+    */
+    public function index(MovieRepository $movieRepository)
+    {
+        $movies = $movieRepository->transformAll();
+
+        return $this->respond($movies);
+    }
+
+    /**
+    * @Route("/movies", methods="POST")
+    */
+    public function create(Request $request,  MovieRepository $movieRepository, EntityManagerInterface $em)
+    {
+        $request = $this->transformJsonBody($request);
+
+        if (! $request) {
+            return $this->respondValidationError('Please provide a valid request!');
+        }
+
+        // validate the title
+        if (! $request->get('title')) {
+            return $this->respondValidationError('Please provide a title!');
+        }
+
+        // validate the descripcion
+        if (! $request->get('descripcion')) {
+            return $this->respondValidationError('Please provide a descripcion!');
+        }
+
+        // validate the imagen
+        if (! $request->get('imagen')) {
+            return $this->respondValidationError('Please provide a imagen!');
+        }
+
+
+        // persist the new movie
+        $movie = new Movie;
+        $movie->setTitle($request->get('title'));
+        $movie->setCount(0);
+        $movie->setDescripcion($request->get('descripcion'));
+        $movie->setImagen($request->get('imagen'));
+        $em->persist($movie);
+        $em->flush();
+
+        return $this->respondCreated($movieRepository->transform($movie));
+
+    }
+
+    /**
+    * @Route("/movies/{id}/count", methods="POST")
+    */
+    public function increaseCount($id, EntityManagerInterface $em, MovieRepository $movieRepository)
+    {
+        $movie = $movieRepository->find($id);
+
+        if (! $movie) {
+            return $this->respondNotFound();
+        }
+
+        $movie->setCount($movie->getCount() + 1);
+        $em->persist($movie);
+        $em->flush();
+
+        return $this->respond([
+            'count' => $movie->getCount()
+        ]);
+    }
+
+    /**
+    * @Route("/movies/{id}", methods="DELETE")
+    */
+
+    public function delete($id, EntityManagerInterface $em, MovieRepository $movieRepository)
+    {
+        /*if (! $this->isAuthorized()) {
+            return $this->respondUnauthorized();
+        }*/
+
+        $movie = $movieRepository->find($id);
+
+        if (! $movie) {
+            return $this->respondNotFound();
+        }
+
+        $em->remove($movie);
+        $em->flush();
+
+        return $this->respond([]);
+    }
+
+}
---
+++ src/Entity/Movie.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MovieRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=MovieRepository::class)
+ */
+class Movie
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $count;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $descripcion;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $imagen;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescripcion(): ?string
+    {
+        return $this->descripcion;
+    }
+
+    public function setDescripcion(string $descripcion): self
+    {
+        $this->descripcion = $descripcion;
+
+        return $this;
+    }
+
+    public function getImagen(): ?string
+    {
+        return $this->imagen;
+    }
+
+    public function setImagen(string $imagen): self
+    {
+        $this->imagen = $imagen;
+
+        return $this;
+    }
+
+    public function getCount(): ?int
+    {
+        return $this->count;
+    }
+
+    public function setCount(int $count): self
+    {
+        $this->count = $count;
+
+        return $this;
+    }
+}
